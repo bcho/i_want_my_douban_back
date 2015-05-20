7a69990d5a8f6ce05be447bafc25d5109cddbbff
Magine <con@loli.lu>
2015-05-20 10:03:54 +0800

    fix h1 at http://www.douban.com/doumail/

--- douban.js
+++ douban.js
@@ -2,8 +2,13 @@
     // 2015-05-17
     var revertDoumail = function() {
         var doumail = document.querySelector('#top-nav-doumail-link');
+        var _h1 = document.querySelector('h1');
 
         doumail.innerText = doumail.innerText.replace('私信', '豆邮');
+
+        if (_h1.innerText == '私信 原“豆邮”已更名为“私信”, 不影响功能使用') {
+            _h1.innerText = _h1.innerText.replace('私信 原“豆邮”已更名为“私信”, 不影响功能使用', '豆邮');
+        }
     };
 
     revertDoumail();
